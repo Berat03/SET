f2d031ceb3b9b451fae9bc13b058b7cc567cf5b5
Berat <bbulbulwork@gmail.com>
2022-12-23 18:47:42 +0000

    Calculated returns and cumulative returns of a single stock.

--- main.R
+++ main.R
@@ -1,26 +1,18 @@
-# Use FANG data set
-data(FANG) # EXAMPLE DATASET
-?FANG
-
-# Get returns for individual stock components grouped by symbol
-Ra <- FANG |>
-  group_by(symbol) |>
-  tq_transmute(adjusted, periodReturn, period = "monthly", col_rename = "Ra")
-
-# Get returns for SP500 as baseline
-Rb <- "^GSPC" |>
-  tq_get(get  = "stock.prices", from = "2010-01-01", to = "2015-12-31") |>
-  tq_transmute(adjusted, periodReturn, period = "monthly", col_rename = "Rb")
-
-# Merge stock returns with baseline
-RaRb <- left_join(Ra, Rb, by = c("date" = "date"))
+library(tidyquant)
+library(tidyverse)
 
 
-# View options
-tq_performance_fun_options()
+AAPL  <- tq_get("AAPL", get = "stock.prices")
 
-# Get performance metrics
 
-x<- RaRb |>
-  tq_performance(Ra = Ra, Rb = Rb, performance_fun = table.CAPM) #used in market model
+AAPL<- AAPL |>
+  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "daily") |>
+  select(date, close, return) |>
+  mutate(cum_return = cumsum(return))
+
+tq_mutate_fun_options()
+
+
+ggplot(AAPL) +
+  geom_line(aes(x = date, y = cum_return, fill ="blue"))
 
