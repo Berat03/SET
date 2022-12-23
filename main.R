1ce780606216c0515e27368f5ba98cf4f911ab77
Berat <bbulbulwork@gmail.com>
2022-12-23 02:32:40 +0000

    experimenting with tq_ function, easy way to calcualte beta and akpha for ?risk adjusted? market model

--- main.R
+++ main.R
@@ -1,1 +1,26 @@
-x <- c(10, 20)
+# Use FANG data set
+data(FANG) # EXAMPLE DATASET
+?FANG
+
+# Get returns for individual stock components grouped by symbol
+Ra <- FANG |>
+  group_by(symbol) |>
+  tq_transmute(adjusted, periodReturn, period = "monthly", col_rename = "Ra")
+
+# Get returns for SP500 as baseline
+Rb <- "^GSPC" |>
+  tq_get(get  = "stock.prices", from = "2010-01-01", to = "2015-12-31") |>
+  tq_transmute(adjusted, periodReturn, period = "monthly", col_rename = "Rb")
+
+# Merge stock returns with baseline
+RaRb <- left_join(Ra, Rb, by = c("date" = "date"))
+
+
+# View options
+tq_performance_fun_options()
+
+# Get performance metrics
+
+x<- RaRb |>
+  tq_performance(Ra = Ra, Rb = Rb, performance_fun = table.CAPM) #used in market model
+
