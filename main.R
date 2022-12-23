2c456b0ce8ed4481f750b93d79a633e4170a0588
Berat <bbulbulwork@gmail.com>
2022-12-23 19:46:12 +0000

    Success plotting cum_return and return - seems to make sence graphically speaking. But I need to check my returns are correct (calculator w/ indiv dates gives different ans (but correct +/-)).

--- main.R
+++ main.R
@@ -3,25 +3,25 @@ library(tidyverse)
 
 # need to split estimation and event period (+- 2 days?)
 
-AAPL  <- tq_get("AAPL", get = "stock.prices", from = "2021-01-01")
-SP500 <- tq_get("^GSPC", get = "stock.prices", from = "2021-01-01")
+AAPL  <- tq_get("AAPL", get = "stock.prices", from = "2010-01-01", to = "2013-01-01", periodicity = "monthly")
+SP500 <- tq_get("^GSPC", get = "stock.prices", from = "2010-01-01", to = "2013-01-01", periodicity = "monthly")
 
 
 AAPL<- AAPL |> 
-  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "daily") |>
+  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "monthly") |>
   select(date, close, return) |>
   mutate(cum_return = cumsum(return))
 
 
 SP500<- SP500 |>
-  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "daily") |>
+  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "monthly") |>
   select(date, close, return) |>
   mutate(cum_return = cumsum(return))
 
 
-
-ggplot(AAPL) +
-  geom_line(aes(x = date, y = cum_return, color ="red"))+
-  geom_line(data = SP500, aes(x = date, y = cum_return, color ="blue"))
+#graph looks to be working
+ggplot(AAPL, aes(x = date)) + 
+  geom_line(aes(y = return, colour = "return")) + 
+  geom_line(aes(y = cum_return, colour = "cum_return"))
 
 
