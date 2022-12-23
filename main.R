73df55cc705bfb7f783b9fa9d378418d1a886636
Berat <bbulbulwork@gmail.com>
2022-12-23 19:32:43 +0000

    more thorough anaylsis with market, prepping to split data into event and estimation

--- main.R
+++ main.R
@@ -1,18 +1,27 @@
 library(tidyquant)
 library(tidyverse)
 
+# need to split estimation and event period (+- 2 days?)
 
-AAPL  <- tq_get("AAPL", get = "stock.prices")
+AAPL  <- tq_get("AAPL", get = "stock.prices", from = "2021-01-01")
+SP500 <- tq_get("^GSPC", get = "stock.prices", from = "2021-01-01")
 
 
-AAPL<- AAPL |>
+AAPL<- AAPL |> 
   tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "daily") |>
   select(date, close, return) |>
   mutate(cum_return = cumsum(return))
 
-tq_mutate_fun_options()
+
+SP500<- SP500 |>
+  tq_mutate(mutate_fun = periodReturn, col_rename = 'return', period = "daily") |>
+  select(date, close, return) |>
+  mutate(cum_return = cumsum(return))
+
 
 
 ggplot(AAPL) +
-  geom_line(aes(x = date, y = cum_return, fill ="blue"))
+  geom_line(aes(x = date, y = cum_return, color ="red"))+
+  geom_line(data = SP500, aes(x = date, y = cum_return, color ="blue"))
+
 
