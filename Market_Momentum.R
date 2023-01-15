2fc321d672453fb62956e28df0737e06999d12cf
berat03 <bbulbulwork@gmail.com>
2023-01-15 04:13:04 +0000

    Implemented x day moving market average with graph

--- Market_Momentum.R
+++ Market_Momentum.R
@@ -1,17 +1,27 @@
 # Do morket momemtum, using different day averages and option for different stocks
+
 library(tidyquant)
-library(tidyverse)
-library(dypl)
-# User Inputs
-moving_average <- 10
-ticker_stock <- "ATVI" 
-ticker_bench <- "^GSPC" 
-begin_date <- YMD("2018-11-05") 
-end_date <- YMD("2018-11-05") 
+library(tidyverse) # when deploying, individually select packages
+library(tidymod)
 
-stock <- tq_get(ticker_stock, get = "stock.prices", from = begin_date, to = end_date, periodicity = "daily")
+average_period <- 125
+ticker_stock <- "^GSPC" 
+begin_date <- ymd("2022-01-18") 
+end_date <- ymd("2023-01-10") 
+get_begin_info <- begin_date - as.difftime((4 * average_period), unit="days") 
 
-bench <- tq_get(ticker_bench, get = "stock.prices", from = begin_date, to = end_date, periodicity = "daily") |>
-  select(symbol, date, bench_adj = adjusted) |>
-  arrange(date)
+stock <- tq_get(ticker_stock, from = get_begin_info, to = end_date <- ymd("2023-01-10"), periodicity = "daily") |>
+  mutate(moving_average = zoo::rollmean(x = adjusted, k = average_period, fill = NA, align = "right")) |>
+  filter(date > begin_date) |>
+  select(date, Price = adjusted, "Moving Average" = moving_average)
+
+stock <- stock |>
+  pivot_longer(cols = !date, names_to = "Metric", values_to = "Value")
+
+
+
+ggplot(stock, aes(x = date)) +
+  geom_line(aes(y = Value, color = Metric)) +
+  labs(x="", y = "") +
+  theme(legend.position="top")
 
