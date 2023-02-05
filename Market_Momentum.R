043702daada7016a3e4351e16f1c0e99fccf48d9
berat03 <bbulbulwork@gmail.com>
2023-02-05 23:39:51 +0000

    Small changes + removed libraries and comments

--- Market_Momentum.R
+++ Market_Momentum.R
@@ -1,9 +1,3 @@
-# Do morket momemtum, using different day averages and option for different stocks
-
-library(tidyquant)
-library(tidyverse) # when deploying, individually select packages
-library(tidymod)
-
 average_period <- 125
 ticker_stock <- "^GSPC" 
 begin_date <- ymd("2022-01-18") 
@@ -18,10 +12,9 @@ stock <- tq_get(ticker_stock, from = get_begin_info, to = end_date <- ymd("2023-
 stock <- stock |>
   pivot_longer(cols = !date, names_to = "Metric", values_to = "Value")
 
-
-
 ggplot(stock, aes(x = date)) +
   geom_line(aes(y = Value, color = Metric)) +
   labs(x="", y = "") +
   theme(legend.position="top")
+
 
