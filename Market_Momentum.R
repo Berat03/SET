fc8f0cb9522e7bb82bdedbc7f4b511a042c0b711
berat03 <bbulbulwork@gmail.com>
2023-09-12 04:02:45 +0100

    cleaned and added functions to app.r

--- Market_Momentum.R
+++ Market_Momentum.R
@@ -1,19 +1,16 @@
 calc_sma <- function(ticker_stock, from, to, time_period, length){
   stock <- tq_get(ticker_stock, from = from, to = to , periodicity = time_period) |>
     mutate(moving_average = zoo::rollmean(x = adjusted, k = length, fill = NA, align = "right")) |>
-    filter(date > from) |>
-    select(date, Price = adjusted, "Moving Average" = moving_average)
-  
-  stock <- stock |>
+    filter(date > ymd(from)) |> #safer with ymd
+    select(date, Price = adjusted, "Moving Average" = moving_average) |>
     pivot_longer(cols = !date, names_to = "Metric", values_to = "Value")
-  
-  ggplot(stock, aes(x = date)) +
-    geom_line(aes(y = Value, color = Metric)) +
-    labs(x="", y = "") +
-    theme(legend.position="top")
-}
+    return (stock)
+  }
 
 
-calc_sma(ticker_stock = "^GSPC", from = ymd("2022-01-18") , to = ymd("2023-01-10") , time_period = "daily", length = 20)
+result <- calc_sma(ticker_stock = "^GSPC", from = ymd("2022-01-18") , to = ymd("2023-01-10") , time_period = "daily", length = 20)
 
-   
+ggplot(result, aes(x = date)) +
+  geom_line(aes(y = Value, color = Metric)) +
+  labs(x="", y = "") +
+  theme(legend.position="top")
