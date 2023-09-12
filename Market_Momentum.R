6aeb8981473505605e8579404e283c104ed9e511
berat03 <bbulbulwork@gmail.com>
2023-09-12 03:11:59 +0100

    cleaned function code

--- Market_Momentum.R
+++ Market_Momentum.R
@@ -1,20 +1,19 @@
-average_period <- 125
-ticker_stock <- "^GSPC" 
-begin_date <- ymd("2022-01-18") 
-end_date <- ymd("2023-01-10") 
-get_begin_info <- begin_date - as.difftime((4 * average_period), unit="days") 
-
-stock <- tq_get(ticker_stock, from = get_begin_info, to = end_date <- ymd("2023-01-10"), periodicity = "daily") |>
-  mutate(moving_average = zoo::rollmean(x = adjusted, k = average_period, fill = NA, align = "right")) |>
-  filter(date > begin_date) |>
-  select(date, Price = adjusted, "Moving Average" = moving_average)
-
-stock <- stock |>
-  pivot_longer(cols = !date, names_to = "Metric", values_to = "Value")
-
-ggplot(stock, aes(x = date)) +
-  geom_line(aes(y = Value, color = Metric)) +
-  labs(x="", y = "") +
-  theme(legend.position="top")
+calc_sma <- function(ticker_stock, from, to, time_period, length){
+  stock <- tq_get(ticker_stock, from = from, to = to , periodicity = time_period) |>
+    mutate(moving_average = zoo::rollmean(x = adjusted, k = length, fill = NA, align = "right")) |>
+    filter(date > from) |>
+    select(date, Price = adjusted, "Moving Average" = moving_average)
+  
+  stock <- stock |>
+    pivot_longer(cols = !date, names_to = "Metric", values_to = "Value")
+  
+  ggplot(stock, aes(x = date)) +
+    geom_line(aes(y = Value, color = Metric)) +
+    labs(x="", y = "") +
+    theme(legend.position="top")
+}
 
 
+calc_sma(ticker_stock = "^GSPC", from = ymd("2022-01-18") , to = ymd("2023-01-10") , time_period = "daily", length = 20)
+
+   
